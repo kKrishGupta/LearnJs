ebf5a091b54506df4e20e38255a857c6b7ffe23a
kKrishGupta <kg20060220@gmail.com>
2025-08-27 16:38:30 +0530

    practice sets2

---
+++ index1.js
@@ -0,0 +1,23 @@
+// let str ="apna college"
+// console.log(str.length)
+
+// template literals
+// let string = `This is a template literal`
+// console.log(typeof(string))
+
+// let obj ={
+//   item :"pens",
+//   price:20
+// };
+// // console.log("price of "+obj.item+" is "+ obj.price);
+
+// let output = `the cost of ${obj.item} is \t ${obj.price}`;
+// console.log(output)
+
+// console.log(output.length)
+
+
+let str ="apna college"
+// console.log(str.toUpperCase())
+
+console.log(str.replace("apna" ,"mera"))
---
+++ practice_set2/index.js
@@ -0,0 +1,4 @@
+let name = prompt("Enter your name");
+
+let username = "@ "+ name + name.length;
+console.log(username);

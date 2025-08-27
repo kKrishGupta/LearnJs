04d03ba9f7bfb7666f36647abc5aab0768ad14bb
kKrishGupta <kg20060220@gmail.com>
2025-08-27 17:19:34 +0530

    practice sets3

---
+++ practice_set3/index.js
@@ -0,0 +1,15 @@
+// let students  =[85,97,44,37,76,60];
+// let sum=0;
+// let avg =0;
+// let n = students.length;
+// for(let el of students){
+//   sum += el;
+// }
+// console.log("avg  :"+sum/n);
+
+
+// ques 2
+let prices = [250,645,300,900,50];
+for(let el of prices){
+  console.log(el-(el*0.1));
+}

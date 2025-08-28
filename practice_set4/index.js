796ab11a9e1f0ccd56fb24dadbaf4ce686058946
kKrishGupta <kg20060220@gmail.com>
2025-08-28 09:01:18 +0530

    practice sets4

---
+++ practice_set4/index.js
@@ -0,0 +1,28 @@
+// let companies = ["google","microsoft","apple","tesla"];
+// console.log(companies);
+// console.log(companies.shift);
+
+// question 2
+// function countVowel(str){
+//   let count =0;
+// for(const char of str){
+//   if(char == 'a' || char == 'e' || char == 'i'|| char == 'o' || char =='u'){
+// count++;
+//   }
+ 
+// }
+//  console.log(count);
+// }
+// countVowel("apna college");
+
+// question 3
+let countVowel = (str) =>{
+  let count =0;
+  for(const char of str){
+    if(char =='a'|| char =='e'|| char == 'i'|| char =='o'|| char == 'u'){
+count++;
+    }
+  }console.log(count);
+}
+
+countVowel("apna college");

778087db26e4ea85997305c47383a316597eddbd
kKrishGupta <kg20060220@gmail.com>
2025-10-03 08:40:34 +0530

    new added file

---
+++ page4/script.js
@@ -0,0 +1,35 @@
+// let obj = {
+//   name  :"harsh",
+//   age :26,
+//   khaana : "daal chaawal",
+// };
+
+// let aa = "name";
+// obj.aa 
+
+// let obj = {
+//   name : "harsh",
+//   address :{
+//     city : "Bhopal",
+//     pincode :462001,
+//     location :{
+//       lat :23.2,
+//       lng :77.4,
+//     },
+//   },
+// };
+
+// // user.address.location.lng;
+
+// let{lat,lng} = user.address.location;
+
+// let obj = {
+//   name :"harsh",
+//   age :26,
+//   email: "test@test.com"
+// ,
+// };
+
+// for(let key in obj){
+
+// }
---
+++ page5/script.js
@@ -0,0 +1,67 @@
+// let abcd = document.getElementById("abcd");
+// console.dir(abcd);
+// let bcd=  document.getElementsByClassName("hey");
+// console.dir(bcd);
+
+// let abcd = document.querySelectorAll("abcd");
+// let h1 = document.querySelector("h1");
+// console.dir(h1);
+// h1.innerText="hello aap kaise ho";
+// h1.innerText = "<i> hello"
+// let a = document.querySelector("a");
+// console.log(a);
+// a.href = "https://www.google.com";
+// a.setAttribute("href", "https://www.google.com");
+
+// let img = document.querySelector("img");
+// img.setAttribute(
+//   "src",
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyaS9dW15PMOAWf9oneAPmq-Fys0W8w3nPXC7wZ36CROlJ3tkNFb0_9uz5CI7h98cHIzOUupljRsENdsXzDLtFfJ-mMB_KHcl7TjZGD8qoXg"
+// )
+
+// let a = document.querySelector("a");
+// console.log(a.getAttribute("href"));
+
+// dynamic DOM
+// let h1 = document.createElement("h1");
+// h1.innerText = "hello hi";
+// document.body.appendChild(h1);
+
+// let h1= document.createElement("h1");
+// h1.textContext = "hey mai bahar se aaya hu";
+// document.querySelector("div").appendChild(h1);
+
+// let h1=  document.querySelector("h1");
+// // h1.style.color ="red";
+// // h1.style.backgroudColor ="yellow";
+// console.dir(h1);
+// h1.classList.add("hulupulu");
+
+// let buyNow = document.querySelectorAll(".buy-now");
+// console.log(buyNow);
+// let heading = document.querySelector("#heading");
+
+// let lis = document.querySelectorAll("li");
+// lis.forEach(function(val){
+//   console.log(val.textContent);
+
+// });
+
+// for(let i=0;i<lis.length;i++){
+//   console.log(lis[i].textContent);
+// }
+
+// let  p = document.querySelector("p");
+// p.innerHTML = "hello how are you";
+
+// let img = document.querySelector("img");
+// console.log(img.src);
+// console.log(img.getAttribute("src"));
+// img.setAttribute("src","https://images.unsplash.com/photo-1570051008600-b34baa49e751?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZyZWUlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D");
+
+// let btn = document.querySelector("button");
+// btn.addEventListener("click",function(){
+//   alert("button clicked");
+// }
+// )
+// document.querySelector("div").removeChild(elementNode)
